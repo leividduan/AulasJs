8d26c91d57c42fa016d7fcc8e4f541a4d1d82104
Deivid <deivid.cardos@gmail.com>
2020-10-21 23:25:30 -0300

    finishing function chapter

---
+++ Functions/deasfioFuncaoConstrutura.js
@@ -0,0 +1,18 @@
+// MEU TESTE
+// function Pessoa(nome){
+//   this.falar = () => console.log(`Meu nome é ${nome}`)
+// }
+
+// const p1 = new Pessoa('João')
+
+
+function Pessoa(nome){
+  this.nome = nome;
+
+  this.falar = function (){
+    console.log(`Meu nome é ${this.nome}`)
+  }
+}
+
+const p1 = new Pessoa('João')
+p1.falar()
---
+++ Functions/factory1.js
@@ -0,0 +1,10 @@
+// Factory simples
+
+function criarPessoa(){
+  return{
+    nome: 'Deivid',
+    sobrenome: 'Cardoso'
+  }
+}
+
+console.log(criarPessoa())
